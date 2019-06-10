15fd0a361108b65b4a477d6604dc1a71dfb04bef
williampinheirocastro <william.castro@olhonocarro.com.br>
2019-06-09 22:11:25 -0300

    v1.3

--- js/main.js
+++ js/main.js
@@ -3,70 +3,63 @@
     const dados = [
         {
             'id': 1,
-            'foto': 'foto-alberto.jpg',
+            'foto': './images/01.jpeg',
             'nome': 'Alberto',
             'cargo': 'Presidente',
             'idade': 20
         },
         {
             'id': 2,
-            'foto': 'foto-bruno.jpg',
+            'foto': './images/02.jpeg',
             'nome': 'Bruno',
             'cargo': 'Diretor',
             'idade': 21
         },
         {
             'id': 3,
-            'foto': 'foto-helena.jpg',
+            'foto': './images/03.jpeg',
             'nome': 'Helena',
             'cargo': 'Gerente',
             'idade': 22
         },
         {
             'id': 4,
-            'foto': 'foto-fernanda.jpg',
+            'foto': './images/04.jpeg',
             'nome': 'Fernanda',
             'cargo': 'Desenvolvedor',
             'idade': 23
         },
         {
             'id': 5,
-            'foto': 'foto-diego.jpg',
+            'foto': './images/05.jpeg',
             'nome': 'Diego',
             'cargo': 'Desenvolvedor',
             'idade': 24
         },
         {
             'id': 6,
-            'foto': 'foto-iris.jpg',
+            'foto': './images/06.jpeg',
             'nome': 'Iris',
             'cargo': 'Desenvolvedor',
             'idade': 25
         },
         {
             'id': 7,
-            'foto': 'foto-hugo.jpg',
+            'foto': './images/07.jpeg',
             'nome': 'Hugo',
             'cargo': 'Desenvolvedor',
             'idade': 26
         },
         {
             'id': 8,
-            'foto': 'foto-gustavo.jpg',
+            'foto': './images/08.jpeg',
             'nome': 'Gustavo',
             'cargo': 'Desenvolvedor',
             'idade': 27
         },
         {
             'id': 9,
-            'foto': 'foto-gabriel.jpg',
-            'nome': 'Gabriel',
-            'cargo': 'Desenvolvedor',
-            'idade': 28
-        },
-        {
-            'id': 19,
-            'foto': 'foto-gabriel.jpg',
+            'foto': './images/09.jpeg',
             'nome': 'Gabriel',
             'cargo': 'Desenvolvedor',
             'idade': 28
@@ -81,26 +74,7 @@
 
     }
 
-    const showProfileSelected = (event) => {
-
-        let indexProfile = event.currentTarget.id.split("-")[1];
-
-        let profile = dados[indexProfile];
-
-        document.getElementById('nome').innerHTML = profile.nome;
-        document.getElementById('cargo').innerHTML = profile.cargo;
-        document.getElementById('idade').innerHTML = profile.idade;
-
-    }
-
     const createMiniCardCompoenet = (dado, dadoIndex) => {
-
-        //     let divMiniCard = document.createElement('div');
-        //     divMiniCard.className = 'col-12 grid-container';
-        //     divMiniCard.id = `profile-${dado.id}`;
-
-        //     let refereceElement = (document.getElementById(`profile-${parseInt(dado.id)}`)) ? document.getElementById(`profile-${parseInt(dado.id)}`) : document.getElementById('mini-cards');
-        //     refereceElement.appendChild(divMiniCard);
 
         let divMiniCardShadown = document.createElement('div');
         divMiniCardShadown.className = 'card-shadow mini-card-shadow';
@@ -140,7 +114,7 @@
         let img = document.createElement('img');
         img.id = 'image-mini-card';
         img.alt = 'Avatar';
-        img.setAttribute('src', 'https://www.w3schools.com/howto/img_avatar.png');
+        img.setAttribute('src', dado.foto);
         containerImg.appendChild(img);
 
 
@@ -152,6 +126,19 @@
         text.innerHTML = dado.cargo;
         text.id = "office-mini-card";
         divChildrenContainer2.appendChild(text);
+
+    }
+
+    const showProfileSelected = (event) => {
+
+        let indexProfile = event.currentTarget.id.split("-")[1];
+
+        let profile = dados[indexProfile];
+
+        document.getElementById('selected-nome').innerHTML = profile.nome;
+        document.getElementById('selected-cargo').innerHTML = profile.cargo;
+        document.getElementById('selected-idade').innerHTML = profile.idade;
+        document.getElementById('selected-foto').src = profile.foto;
 
     }
 
